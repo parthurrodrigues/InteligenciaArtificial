e32309e57016c2547ed680fff984577ffcf1882e
Pedro <pedroarthur61@hotmail.com>
2023-06-02 17:41:43 -0300

    Commit

---
+++ app/src/main/java/com/example/inteligenciaartificial/Usuario.kt
@@ -0,0 +1,20 @@
+package com.example.inteligenciaartificial
+
+class Usuario {
+    var nome: String = ""
+    var email: String = ""
+    var usuario: String = ""
+    var senha: String = ""
+    var opiniao: String = ""
+
+    constructor() {
+    }
+
+    constructor(nome: String, email: String, usuario: String, senha: String, opiniao: String) {
+        this.nome = nome
+        this.email = email
+        this.usuario = usuario
+        this.senha = senha
+        this.opiniao = opiniao
+    }
+}
